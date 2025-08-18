588a5466e6838958089a5e2211a82e3765b6bb1e
SYasmin464 <yasminshakhina@gmail.com>
2025-08-18 09:14:55 -0600

    featuresun added

--- src/main/java/SolarSystem.java
+++ src/main/java/SolarSystem.java
@@ -7,5 +7,10 @@ public class SolarSystem {
         BB.Stars="10";
         BB.Sun="1";
         System.out.println("Solar system is ready");
+
+        FeatureSun Xros=new FeatureSun();
+        Xros.temp="34324k";
+        Xros.Color="green";
+        Xros.Radius="34234234324miles";
     }
 }
